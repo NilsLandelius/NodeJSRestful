6177aa9e895e5e1265cdc29cfb8bd3f0f052c8ae
Nils <nilsoskar.landelius@gmail.com>
2019-02-28 18:25:03 +0100

    preparing app for heroku deployment

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
+if(process.env.NODE_ENV = 'production')
+require('./startup/prod')(app);
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
